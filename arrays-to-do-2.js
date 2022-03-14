7b5652d719f4beab0df3c77ceb93108d2ba72b24
neomarwilliams <neoma.rae@gmail.com>
2022-03-13 23:20:33 -0700

    to-dos 2 modified

--- arrays-to-do-2.js
+++ arrays-to-do-2.js
@@ -1,4 +1,37 @@
 // Reverse-- change the order of the values in place
+function reverse(arr) {
+    console.log(arr)
+    for(var i = 0; i < arr.length/2; i++) {
+        var temp = arr[i]
+        arr[i] = arr[arr.length -1 - i]
+        arr[arr.length -1 -i] = temp
+    }
+    console.log(arr)
+    return arr
+}
+
+reverse([1,2,3,4])
+reverse([12, 4, 6, 9, 9])
+
+
+
+
 // Rotate-- offset array values by given argument
+function rotate(arr, numToOffsetBy) {
+    pass
+}
+
+
 // Filter Range-- keep only the values between a min and max
+function filterRange(arr, min, max) {
+    var toRemoveCount = 0
+    for(var i = 0; i < arr.length; i++) {
+        if(arr[i] < min || arr[i] > max) {
+
+            toRemoveCount += 1
+            //move it to the end, then add to var count
+        }
+    }
+    // move thing to the end
+}
 // Concat-- replicate JS's concat() function
